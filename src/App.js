1342e49eb402ae13e21768eb5aea685322d6d47c
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-21 18:17:28 -0400

    Set up state for API data. Got API data on mount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
 const API_KEY = "k5sSIJGot1xNH0t5AafhbkAW49Cb6MyJ8YHZ1OTD";
+const API_URL = "https://api.nasa.gov/planetary/apod?api_key="
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(()=>{
+    axios.get(`${API_URL}${API_KEY}`).then(({data})=>{
+      console.log(data);
+    }).catch(err=>{
+      console.log(err);
+    })
+  }, []);
+
   return (
     <div className="App">
       <h1>NASA APOD</h1>
