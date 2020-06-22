6c1beeef0a17a621ac9b677d96b2ca4cc2c01efe
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-22 04:35:18 -0400

    Changed video tag to iframe for youtube videos

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Media({mediaLink, mediaType}){
     let media;
     if(mediaType === "image")  media = <img alt="nasa astronomy photo of the day" src={mediaLink}/>;
-    else media = <video><source src={mediaLink} type="video/mp4"></source></video>;
+    else media = <iframe src={mediaLink}></iframe>;
     return (
         <div id="apod">
             {media}
