e64ce5a81eb3c3613357466f695fa510800a14ef
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-21 20:06:55 -0400

    Created Date and Description components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import axios from "axios";
 import "./App.css";
 
 import Title from './components/Title';
+import Date from './components/Date';
 import Photo from './components/Photo';
+import Description from './components/Description';
 
 const API_KEY = "k5sSIJGot1xNH0t5AafhbkAW49Cb6MyJ8YHZ1OTD";
 const API_URL = "https://api.nasa.gov/planetary/apod?api_key="
@@ -26,7 +28,9 @@ function App() {
       <>
         <h1>NASA APOD</h1>
         <Title title={data.title}/>
+        <Date date={data.date}/>
         <Photo photo={data.hdurl}/>
+        <Description description={data.explanation}/>
       </>}
     </div>
   );
--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Photo({photo}){
-    console.log(photo)
     return (
         <img alt="nasa astronomy photo of the day" src={photo}/>
     );
