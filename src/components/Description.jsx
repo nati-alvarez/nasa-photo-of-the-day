cff6a6b615e66e8596ee6ea104bf18dcfe193e32
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-24 19:46:44 -0400

    Made styled component for description component

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styledComponents from 'styled-components';
+
+const Paragraph = styledComponents.p`
+    max-width: 800px;
+    margin: 0 auto;
+    margin-top: 5vh;
+    margin-bottom: 5vh;
+    line-height: 1.4rem;
+    background: rgba(0, 0, 0, .6);
+    padding: 20px;
+    border-radius: 5px;
+`;
 
 export default function Description({description}){
     return (
-        <p className="description">{description}</p>
+        <Paragraph className="description">{description}</Paragraph>
     );
 }
