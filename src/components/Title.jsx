32030af4c79131a3e8f9ef21a07a17160175dce8
Natividad Alvarez <tivialvarez@gmail.com>
2020-06-24 21:05:47 -0400

    Created styled component for Title component

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styledComponent from 'styled-components';
+
+const Heading= styledComponent.h2`
+    text-align: center;
+`;
 
 export default function Title({title}){
     return (
-    <h2>{title}</h2>
+    <Heading>{title}</Heading>
     )
 }
